403a6d5ad76610be26b58162168272844ed40a46
SSProgamer <72447877+SSProgamer@users.noreply.github.com>
2022-10-27 15:40:24 +0700

    Main Nav and Footer

--- index.php
+++ index.php
@@ -6,45 +6,54 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Good Game Merchandise</title>
     <link href="stylesmainpage.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-        crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
     <div class="container-fluid main-container">
         <nav class="navbar fixed-top main-navbar shadow-sm border border-dark">
             <ul class="nav me-auto ms-5">
-                <li class="nav-item">
-                    <h1 class="text-light" onclick="parent.location='https\:\/\/www.google.com/?hl=th&authuser=1'">
-                        GoodGame</h1>
+                <li class="nav-item ms-5">
+                    <h1><a class="nav-link text-white" href="https://www.youtube.com/watch?v=cErgMJSgpv0">GoodGame</a></h1>
                 </li>
-                <li class="nav-item ms-3 me-5">
-                    <a class="nav-link web-text-color fw-bold text-in-nav" href="">Box Sets</a>
+                <li class="nav-item me-5">
+                    <a class="nav-link web-text-color fw-bold text-in-nav" href="https://www.youtube.com/watch?v=cErgMJSgpv0">Box Sets</a>
                 </li>
                 <li class="nav-item ms-5">
-                    <a class="nav-link web-text-color fw-bold text-in-nav" href="">Merchandises</a>
+                    <a class="nav-link web-text-color fw-bold text-in-nav" href="https://www.youtube.com/watch?v=cErgMJSgpv0">Merchandises</a>
                 </li>
             </ul>
             <form class="d-flex text-in-nav search-nav" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <input class="form-control me-2 text-white bg-dark fw-bold" type="search" placeholder="Search" aria-label="Search">
             </form>
             <ul class="nav me-5">
                 <li class="nav-item">
-                    <a class="nav-link web-text-color fw-bold" href="">Sign Up</a>
+                    <a class="nav-link web-text-color fw-bold" href="https://www.youtube.com/watch?v=cErgMJSgpv0">Sign Up</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link web-text-color fw-bold" href="">Log In</a>
+                    <a class="nav-link web-text-color fw-bold" href="https://www.youtube.com/watch?v=cErgMJSgpv0">Log In</a>
                 </li>
             </ul>
         </nav>
 
     </div>
-    <footer class="card-footer main-footer">
-        <div class="container-fluid">
-            <h1>test</h1>
+    <footer class="main-navbar">
+        <div class="container-fluid p-5">
+            <div class="row">
+                <div class="col-3">
+                    <h1><a class="nav-link text-white" href="https://www.youtube.com/watch?v=cErgMJSgpv0">GoodGame</a></h1>
+                </div>
+                <div class="col-7">
+                    <a class="nav-link web-text-color pb-3" href="https://www.youtube.com/watch?v=cErgMJSgpv0">Box Set</a>
+                    <a class="nav-link web-text-color pb-3" href="https://www.youtube.com/watch?v=cErgMJSgpv0">Merchandises</a>
+                    <a class="nav-link web-text-color pb-3" href="https://www.youtube.com/watch?v=cErgMJSgpv0">All Products</a>
+                    <a class="nav-link web-text-color" href="https://www.youtube.com/watch?v=cErgMJSgpv0">Products Popular</a>
+                </div>
+                <div class="col-2">
+                    <p class="text-white">We sell many goody</p>
+                </div>
+            </div>
         </div>
     </footer>
 </body>
