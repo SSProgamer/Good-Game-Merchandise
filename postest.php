6c8c88968ffd03cdae3a8891a8e3d4db85b02137
Baipor <baipor2545@hotmail.com>
2022-11-15 00:12:17 +0700

    fixed bug
    กำลังแก้บัคแก้ไขที่อยู่มันไม่ยอมแก้ให้

---
+++ postest.php
@@ -0,0 +1,10 @@
+<?php
+include "navbar.php";
+$sql2 = 'UPDATE Customer
+SET "FirstName" = "'.$_POST['fname'].'", "LastName" = "'.$_POST['lname'].'", "Address"= "'.$_POST['address'].'", "City"= "'.$_POST['city'].'","Province"="'.$_POST['province'].'",Postcode="'.$_POST['postcode'].'",phonenumber="'.$_POST['phone'].'"
+WHERE "CustomerID" = "'.$_SESSION["ID"].'"';
+$result2 = $db->query($sql2);
+if(!$result2){
+    echo "bruh";
+}
+?>
--- user.php
+++ user.php
@@ -6,10 +6,11 @@ if (!$result) {
     // echo "bruh";
     header("location: login.php");
 }
-if (isset($_POST["submit"])) {
+if(isset($_POST["submit"])){
+    #
     $sql2 = 'UPDATE Customer
-    SET FirstName = "' . $_POST['fname'] . '", LastName = "' . $_POST['lname'] . '", Address= "' . $_POST['address'] . '", City= "' . $_POST['city'] . '",Province="' . $_POST['province'] . '",Postcode="' . $_POST['postcode'] . '",phonenumber="' . $_POST['phone'] . '"
-    WHERE CustomerID = "' . $_SESSION["ID"] . '"';
+    SET "FirstName" = "'.$_POST['fname'].'", "LastName" = "'.$_POST['lname'].'", "Address"= "'.$_POST['address'].'", "City"= "'.$_POST['city'].'","Province"="'.$_POST['province'].'",Postcode="'.$_POST['postcode'].'",phonenumber="'.$_POST['phone'].'"
+    WHERE "CustomerID" = "'.$_SESSION["ID"].'"';
     $result2 = $db->query($sql2);
     if (!$result2) {
         // 
