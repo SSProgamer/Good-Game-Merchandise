1391c36df6520757ca4ef10155e333859c244444
Baipor <baipor2545@hotmail.com>
2022-11-14 16:48:35 +0700

    uodate  navbar

--- navbar.php
+++ navbar.php
@@ -1,17 +1,21 @@
 <?php
 session_start();
+include "db_connect.php";
+
 if (!isset($_SESSION["cart"])) {
     $_SESSION["cart"] = array();
 }
-include "db_connect.php";
+
 if (isset($_SESSION["email"])) {
 }
 //ถ้ากดปุ่มLogout
 if (isset($_POST['logout'])) {
     unset($_SESSION["email"]); //unsetตัวแปรSession
     unset($_SESSION["ID"]);
+    // header("location: index.php");
 }
 ?>
+
 <html lang="en">
 
 <head>
@@ -56,10 +60,27 @@ if (isset($_POST['logout'])) {
             } else {
                 //if set will show username or email just like you want
                 $sent = htmlspecialchars($_SERVER["PHP_SELF"]);
-                echo '<a class="nav-link web-text-color fw-bold" href="#">Welcome, ' . $_SESSION["email"] . '</a>';
-                echo '<form action="' . $sent . '" method="post">
-                        <button type="submit" name="logout">logout</button>
-                        </form>';
+                echo '<a class="nav-link web-text-color fw-bold" href="user.php">Welcome, ' . $_SESSION["email"] . '</a>';
+                // echo '<form action="' . $sent . '" method="post">
+                //         <button type="submit" name="logout">logout</button>
+                //         </form>';
+                echo '<a href="#"><img src="image/webelement/cart.png" alt="" class="me-3"></a>';
+                echo '<div class="dropdown">
+                <a class="btn btn-secondary dropdown-toggle user-dropdown" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="image/webelement/user.png" alt="">
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end user-dropdown-list shadow-sm">
+                    <li><a class="dropdown-item text-white user-dropdown-list-item" href="user.php">Account</a></li>
+                    <li><a class="dropdown-item text-white user-dropdown-list-item" href="purchases.php">Purchases</a></li>
+                    <li><a class="dropdown-item text-white user-dropdown-list-item" href="wishlist.php">Wish List</a></li>
+                    <li>
+                        <form action="' . $sent . '" method="post">
+                            <input type="hidden" name="logout">
+                            <a class="dropdown-item text-white user-dropdown-list-item" onclick="this.parentNode.submit();">Log Out</a>
+                        </form>
+                    </li>
+                </ul>
+            </div>';
             }
             ?>
         </ul>
--- user.php
+++ user.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+include "navbar.php";
+$sql = "SELECT * FROM Customer WHERE CustomerID =" . $_SESSION["ID"] . "";
+$result = $db->query($sql);
+if(!$result){
+    echo "bruh";
+    header("location: login.php");
+}
+
+?>
+
 <html lang="en">
 
 <head>
@@ -7,8 +19,8 @@
     <title>User</title>
     <link rel="stylesheet" href="stylesuser.css">
     <link rel="stylesheet" href="stylesnavfoot.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch&display=swap" rel="stylesheet">
@@ -16,7 +28,7 @@
 
 <body>
     <!-- navbar -->
-    <nav class="navbar sticky-top main-navbar shadow-sm border border-dark">
+    <!-- <nav class="navbar sticky-top main-navbar shadow-sm border border-dark">
         <ul class="nav me-auto ms-5">
             <li class="nav-item ms-5 mt-1">
                 <h3><a class="nav-link text-white" href="index.php">GoodGame</a></h3>
@@ -45,9 +57,10 @@
                 </ul>
             </div>
         </div>
-    </nav>
-
-    <div class="container-fluid main-container">
+    </nav> -->
+    <?php
+    while ($info = $result->fetchArray(SQLITE3_ASSOC)) {
+        echo '<div class="container-fluid main-container">
         <div class="container">
             <h2 class="mt-3 mb-3 fw-bold">Account Information</h2>
             <div class="container account-div p-5 mb-5 rounded">
@@ -56,7 +69,7 @@
                         <h5 class="fw-bold text-end"><label for="infoemail" class="col-form-label">Email Address</label></h5>
                     </div>
                     <div class="col-9">
-                        <input type="email" id="infoemail" class="form-control">
+                        <input type="email" id="infoemail" class="form-control" value="'.$info["Email"].'" disabled>
                     </div>
                 </div>
                 <div class="row align-items-center mb-3">
@@ -64,7 +77,7 @@
                         <h5 class="fw-bold text-end"><label for="infofname" class="col-form-label">Firstname</label></h5>
                     </div>
                     <div class="col-9">
-                        <input type="text" id="infofname" class="form-control" disabled>
+                        <input type="text" id="infofname" class="form-control" value="'.$info["Firstname"].'" disabled>
                     </div>
                 </div>
                 <div class="row align-items-center mb-3">
@@ -72,7 +85,7 @@
                         <h5 class="fw-bold text-end"><label for="infolname" class="col-form-label">Lastname</label></h5>
                     </div>
                     <div class="col-9">
-                        <input type="text" id="infolname" class="form-control" disabled>
+                        <input type="text" id="infolname" class="form-control" value="'.$info["Lastname"].'" disabled>
                     </div>
                 </div>
                 <div class="row align-items-center mb-3">
@@ -80,7 +93,7 @@
                         <h5 class="fw-bold text-end"><label for="infoaddress" class="col-form-label">Address</label></h5>
                     </div>
                     <div class="col-9">
-                        <input type="text" id="infoaddress" class="form-control" disabled>
+                        <input type="text" id="infoaddress" class="form-control" value="'.$info["Address"].'" disabled>
                     </div>
                 </div>
                 <div class="row align-items-center mb-3">
@@ -88,7 +101,7 @@
                         <h5 class="fw-bold text-end"><label for="infocity" class="col-form-label">City</label></h5>
                     </div>
                     <div class="col-9">
-                        <input type="text" id="infocity" class="form-control" disabled>
+                        <input type="text" id="infocity" class="form-control" value="'.$info["City"].'" disabled>
                     </div>
                 </div>
                 <div class="row align-items-center mb-3">
@@ -96,7 +109,7 @@
                         <h5 class="fw-bold text-end"><label for="infoprovince" class="col-form-label">Province</label></h5>
                     </div>
                     <div class="col-9">
-                        <input type="text" id="infoprovince" class="form-control" disabled>
+                        <input type="text" id="infoprovince" class="form-control" value="'.$info["Province"].'" disabled>
                     </div>
                 </div>
                 <div class="row align-items-center mb-3">
@@ -104,7 +117,7 @@
                         <h5 class="fw-bold text-end"><label for="infopostcode" class="col-form-label">Postcode</label></h5>
                     </div>
                     <div class="col-9">
-                        <input type="text" id="infopostcode" class="form-control" disabled>
+                        <input type="text" id="infopostcode" class="form-control" value="'.$info["Postcode"].'" disabled>
                     </div>
                 </div>
                 <div class="row align-items-center mb-4">
@@ -112,14 +125,15 @@
                         <h5 class="fw-bold text-end"><label for="infophone" class="col-form-label">Phone Number</label></h5>
                     </div>
                     <div class="col-9">
-                        <input type="text" id="infophone" class="form-control" disabled>
+                        <input type="text" id="infophone" class="form-control" value="'.$info["phonenumber"].'" disabled>
                     </div>
-                </div>
-                <div class="d-flex justify-content-end">
-                    <button type="button" class="btn btn-primary change-button">Update</button>
-                </div>
-            </div>
-        </div>
+                </div>';
+    } ?>
+    <div class="d-flex justify-content-end">
+        <button type="button" class="btn btn-primary change-button">Update</button>
+    </div>
+    </div>
+    </div>
     </div>
 
     <!-- footer -->
