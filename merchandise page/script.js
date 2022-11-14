f68de24568ccdac20bc598e9833f8a7653e48c25
GasidisHamongkol <64070003@kmitl.ac.th>
2022-11-14 17:43:03 +0700

    Merchandise Page semi done
    idk man it's semi done

--- merchandise page/script.js
+++ merchandise page/script.js
@@ -1,4 +1,8 @@
 let items = document.querySelectorAll('.carousel .carousel-item')
+var overlay = document.getElementById("overlay");
+var popup = document.getElementById("popup");
+var button = document.getElementById("button");
+var popupaddcart = document.getElementById("popupaddcart");
 
 items.forEach((el) => {
     const minPerSlide = 5
@@ -13,3 +17,16 @@ items.forEach((el) => {
         next = next.nextElementSibling
     }
 })
+function purchasePopUp(){
+    overlay.style.display = 'block';
+	popup.style.display = 'block';
+}
+function addtocartPopUp(){
+    overlay.style.display = 'block';
+	popupaddcart.style.display = 'block';
+}
+function closepopup() {
+    overlay.style.display = 'none';
+        popup.style.display = 'none';
+        popupaddcart.style.display = 'none';
+  };
