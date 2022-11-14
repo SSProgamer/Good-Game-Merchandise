17512e54aeb2e536251176df30df797c860c4a04
chartnayodom <88505381+chartnayodom@users.noreply.github.com>
2022-11-15 00:28:25 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 include 'db_connect.php';
-if (isset($_POST['submit'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
     //
