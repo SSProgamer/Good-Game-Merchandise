5462ac7a8c457a19d1483043058e925ad5ad2d9b
Baipor <baipor2545@hotmail.com>
2022-11-18 20:33:11 +0700

    add reset password page

---
+++ resetpw.php
@@ -0,0 +1,20 @@
+<?php
+    include "db_connect.php";
+    //หน้าreset password
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <input type="password" name="password" id="password">
+        <input type="password" name="c_password" id="c_password">
+        <button type="submit">Reset</button>
+    </form>
+</body>
+</html>
