0ee95d031758d7a62a31e55f2cb38ff7d9cf57dc
Baipor <baipor2545@hotmail.com>
2022-11-18 17:39:56 +0700

    add send receipt but not done yet.

---
+++ sendreceipt.php
@@ -0,0 +1,27 @@
+<?php
+    include "navbar.php";
+?>
+<html lang="en">
+<head>
+    <title>Send Reciept</title>
+</head>
+<body>
+    <select name="order" id="order" form="receipt" required>
+            <option value=""></option>
+            <option value="1">1</option>
+            <option value="1">1</option>
+            <option value="1">1</option>
+            <option value="1">1</option>
+            <option value="1">1</option>
+        </select>
+    <form action="" method="post" id="receipt">
+        
+        <input type="file" name="" id="">
+        <input type="text" name="" id="">
+        <input type="text" name="" id="">
+        <input type="datetime-local" name="" id="">
+        <button type="submit">Submit your reciept</button>
+        
+    </form>
+</body>
+</html>
